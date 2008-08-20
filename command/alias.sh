2c95421be426637152d19a598a8f59eba0615625
R. Bernstein <rocky@gnu.org>
2008-08-20 08:14:15 -0400

    Add print and eval aliases. Administrivia.

--- command/alias.sh
+++ command/alias.sh
@@ -3,22 +3,25 @@
 #
 #   Copyright (C) 2008 Rocky Bernstein rocky@gnu.org
 #
-#   bashdb is free software; you can redistribute it and/or modify it under
+#   kshdb is free software; you can redistribute it and/or modify it under
 #   the terms of the GNU General Public License as published by the Free
 #   Software Foundation; either version 2, or (at your option) any later
 #   version.
 #
-#   bashdb is distributed in the hope that it will be useful, but WITHOUT ANY
+#   kshdb is distributed in the hope that it will be useful, but WITHOUT ANY
 #   WARRANTY; without even the implied warranty of MERCHANTABILITY or
 #   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 #   for more details.
 #   
 #   You should have received a copy of the GNU General Public License along
-#   with bashdb; see the file COPYING.  If not, write to the Free Software
+#   with kshdb; see the file COPYING.  If not, write to the Free Software
 #   Foundation, 59 Temple Place, Suite 330, Boston, MA 02111 USA.
 
 _Dbg_help_add alias \
-'alias name debugger-command	Make name be an alias for debugger-command.'
+'alias NAME DEBUGGER-COMMAND	-- Make NAME be an alias for DEBUGGER-COMMAND.
+
+Use "unalias" to remove an alias and "show aliases" to get a list the
+aliases in effect.'
 
 _Dbg_do_alias() {
   if (($# != 2)) ; then
@@ -28,7 +31,9 @@ _Dbg_do_alias() {
 }
 
 _Dbg_help_add unalias \
-'unalias NAME	-- Remove debugger command alias NAME.'
+'unalias NAME	-- Remove debugger command alias NAME.
+
+Use "show aliases" to get a list the aliases in effect.'
 
 _Dbg_do_unalias() {
   if (($# != 1)) ; then
--- command/eval.sh
+++ command/eval.sh
@@ -3,18 +3,18 @@
 #
 #   Copyright (C) 2008 Rocky Bernstein rocky@gnu.org
 #
-#   zshdb is free software; you can redistribute it and/or modify it under
+#   kshdb is free software; you can redistribute it and/or modify it under
 #   the terms of the GNU General Public License as published by the Free
 #   Software Foundation; either version 2, or (at your option) any later
 #   version.
 #
-#   zshdb is distributed in the hope that it will be useful, but WITHOUT ANY
+#   kshdb is distributed in the hope that it will be useful, but WITHOUT ANY
 #   WARRANTY; without even the implied warranty of MERCHANTABILITY or
 #   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 #   for more details.
 #   
 #   You should have received a copy of the GNU General Public License along
-#   with zshdb; see the file COPYING.  If not, write to the Free Software
+#   with kshdb; see the file COPYING.  If not, write to the Free Software
 #   Foundation, 59 Temple Place, Suite 330, Boston, MA 02111 USA.
 
 # temp file for internal eval'd commands
@@ -39,6 +39,8 @@ _Dbg_do_eval() {
   _Dbg_set_debugger_internal
 }
 
+_Dbg_alias_add 'e' 'eval'
+
 # The arguments in the last "print" command.
 typeset _Dbg_last_print_args=''
 
@@ -52,3 +54,5 @@ _Dbg_do_print() {
 
   _Dbg_do_eval _Dbg_msg "$_Dbg_expr"
 }
+
+_Dbg_alias_add 'p' 'print'
