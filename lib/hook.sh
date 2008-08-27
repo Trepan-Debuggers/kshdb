3715bc1516c4c93b0455b7397dbf452e04d69763
R. Bernstein <rocky@gnu.org>
2008-08-26 21:02:11 -0400

    More common with zshdb

--- lib/hook.sh
+++ lib/hook.sh
@@ -61,7 +61,7 @@ function _Dbg_debug_trap_handler {
 	    typeset -i _Dbg_frame_previous_lineno="$_Dbg_frame_last_lineno"
 	    _Dbg_frame_save_frames 1
 	    if ((_Dbg_frame_previous_lineno == _Dbg_frame_last_lineno)) && \
-		[ $_Dbg_frame_previous_file == $_Dbg_frame_last_file ] ; then
+		[ "$_Dbg_frame_previous_file" = "$_Dbg_frame_last_file" ] ; then
 		_Dbg_set_to_return_from_debugger 1
 		return $_Dbg_rc
 	    fi
