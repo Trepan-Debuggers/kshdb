3bfcd1c796f3eda364aa13bcd34c66e840b325f5
R. Bernstein <rocky@gnu.org>
2008-12-12 17:19:33 -0500

    Add a restart test (prior ksh's would crash on the test suite).

---
+++ test/data/restart.cmd
@@ -0,0 +1,10 @@
+set trace-commands on
+# Test Restart command
+list
+step
+step
+break 5
+restart -B -q -L ../.. -x ../../test/data/restart2.cmd ../../test/example/restart.sh
+# We never get here
+print You should not see this.
+quit 
---
+++ test/example/restart.sh
@@ -0,0 +1,7 @@
+#!/bin/ksh
+# For testing restart.
+
+x=1
+cd /
+y=2
+z=0
