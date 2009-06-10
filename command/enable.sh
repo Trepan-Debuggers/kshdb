4e6b2449662d04ec03c89382dce3f96bd2a02353
R. Bernstein <rocky@gnu.org>
2009-06-10 12:59:41 -0400

    Add debugger breakpoing commands: delete, disable and examine.

---
+++ command/enable.sh
@@ -0,0 +1,26 @@
+# -*- shell-script -*-
+# enable.sh - gdb-like "enable" debugger command
+#
+#   Copyright (C) 2008, 2009 Rocky Bernstein rocky@gnu.org
+#
+#   kshdb is free software; you can redistribute it and/or modify it under
+#   the terms of the GNU General Public License as published by the Free
+#   Software Foundation; either version 2, or (at your option) any later
+#   version.
+#
+#   kshdb is distributed in the hope that it will be useful, but WITHOUT ANY
+#   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#   for more details.
+#   
+#   You should have received a copy of the GNU General Public License along
+#   with kshdb; see the file COPYING.  If not, write to the Free Software
+#   Foundation, 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+
+_Dbg_help_add enable \
+'enable {N}... -- Enable breakpoint entry/entries.'
+
+# Enable breakpoint(s)/watchpoint(s) by entry number(s).
+_Dbg_do_enable() {
+  _Dbg_enable_disable 1 'enabled' $@
+}
