27326d883a0995952530cf0650168ccf75b4a956
rocky <rocky@sanchez.(none)>
2009-06-18 20:54:18 -0400

    Add examine unit test.

---
+++ test/example/exam.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+# For testing examine.
+ary=(10 100 100)
+string='a string'
+export xstring='an exported string'
+readonly ary
+fn() { print "a function"; }
+z=1
+unset string
+r='abc'
+
