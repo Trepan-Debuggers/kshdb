c0a849ec82994bb0e248f247e3703bf8f45fdbc3
rocky <rocky@gnu.org>
2010-02-12 21:30:50 -0500

    make distcheck (building outside of source tree) should now work.

--- command/break.sh
+++ command/break.sh
@@ -1,5 +1,5 @@
 # -*- shell-script -*-
-#   Copyright (C) 2008, 2009 Rocky Bernstein rocky@gnu.org
+#   Copyright (C) 2008, 2009, 2010 Rocky Bernstein rocky@gnu.org
 #
 #   kshdb is free software; you can redistribute it and/or modify it under
 #   the terms of the GNU General Public License as published by the Free
