227747a236ac09d071509a4cdc02c780d9886484
rocky <rocky@gnu.org>
2011-04-01 15:40:18 -0400

    Add assignment expressions to ev?

--- lib/fns.sh
+++ lib/fns.sh
@@ -81,6 +81,7 @@ _Dbg_eval_re=(
     '^[ \t]*(if|elif)[ \t]+([^;]*)((;[ \t]*then?)?|$)'
     '^[ \t]*return[ \t]+(.*)$'
     '^[ \t]*while[ \t]+([^;]*)((;[ \t]*do?)?|$)'
+    '^[ \t]*[A-Za-z_][A-Za-z_0-9[]*]?[-+]?=(.*$)'
 )
 
 # Removes "[el]if" .. "; then" or "while" .. "; do" or "return .."
@@ -96,6 +97,8 @@ _Dbg_eval_extract_condition()
 	extracted="echo ${.sh.match[1]}"
     elif [[ $orig =~ ${_Dbg_eval_re[2]} ]] ; then
 	extracted=${.sh.match[1]}
+    elif [[ $orig =~ ${_Dbg_eval_re[3]} ]] ; then
+	extracted="echo ${.sh.match[1]}"
     else
 	extracted=$orig
     fi
