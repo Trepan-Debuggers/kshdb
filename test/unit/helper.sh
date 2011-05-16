333a68e8461eb51053ab1ff7b390fe24d6897ae5
rocky <rocky@gnu.org>
2011-05-15 21:01:33 -0400

    DRY unit test code using a helper routine. More is needed...

---
+++ test/unit/helper.sh
@@ -0,0 +1,7 @@
+PS4='(${.sh.file}:${LINENO}): ${.sh.fun} - [${.sh.subshell}] 
+'
+_Dbg_libdir=$abs_top_srcdir
+
+shunit_file=${abs_top_srcdir}test/unit/shunit2
+# Don't need to show banner
+set -- '-q'  
