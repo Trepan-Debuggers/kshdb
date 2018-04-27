a60171b91cae5277fb7c1e31ca7f0777dbc8e48a
rocky <rocky@gnu.org>
2018-04-27 12:42:14 -0400

    where command -> backtrace command

--- command/backtrace.sh
+++ command/backtrace.sh
@@ -1,7 +1,7 @@
 # -*- shell-script -*-
-# where.cmd - gdb-like "backtrace" debugger command
+# backtrace.cmd - gdb-like "backtrace" debugger command
 #
-#   Copyright (C) 2008, 2016 Rocky Bernstein rocky@gnu.org
+#   Copyright (C) 2008, 2016, 2018 Rocky Bernstein rocky@gnu.org
 #
 #   kshdb is free software; you can redistribute it and/or modify it under
 #   the terms of the GNU General Public License as published by the Free
